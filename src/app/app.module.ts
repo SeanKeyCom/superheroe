16356c6e64d24f4cdede10aaa1e84575dd1526e6
SeanKeyCom <123166387+SeanKeyCom@users.noreply.github.com>
2023-02-18 21:15:48 +0100

    Updated project to most modern versions of packages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { NgModule, Directive } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuperheroeSearchComponent } from './modules/superheroe-search/superheroe-search.component';
 import { SuperheroeManagementComponent } from './modules/superheroe-management/superheroe-management.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DeleteDialogComponent } from './shared/components/dialog-modal/delete-dialog/delete-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ToUpperCaseDirective } from './shared/directives/to-upper-case.directive';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { ToUpperCaseKeyupDirective } from './shared/directives/to-upper-case-keyup.directive';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,7 @@ import { ToUpperCaseKeyupDirective } from './shared/directives/to-upper-case-key
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    //BrowserAnimationsModule,
     MatPaginatorModule,
     MatTableModule,
     MatFormFieldModule,
